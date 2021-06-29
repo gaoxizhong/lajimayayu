7e09f7906b0e2cf091e5e7cab77f78e72c22f4b4
gaoxizhong <296203386@qq.com>
2021-06-29 10:40:43 +0800

    1
    11

--- assets/js/wx_request.js
+++ assets/js/wx_request.js
@@ -1,32 +1,32 @@
 const host = 'https://v.ljmyy.com/api' // 将域名保存到一个变量中
 class wx_request{
-  request (url, method, data, header = {}) {
-    wx.showLoading({
-        title: '加载中...',
-        mask: true,
-    });
-    return new Promise((resolve,reject) => {
-        wx.request({
-            url:host + url,
-            method:method,
-            data:data,
-            headers:{
-                'content-type': 'application/json' // 默认值
-            },
-            success:function(res) {
-                wx.hideLoading()
-                resolve(res.data)
-            },
-            fail: function (res) {
-                wx.hideLoading()
-                reject(res)
-              },
-              complete: function () {
-                wx.hideLoading()
-              }
+    request (url, method, data, header = {}) {
+        wx.showLoading({
+            title: '加载中...',
+            mask: true,
+        });
+        return new Promise((resolve,reject) => {
+            wx.request({
+                url:host + url,
+                method:method,
+                data:data,
+                headers:{
+                    'content-type': 'application/json' // 默认值
+                },
+                success:function(res) {
+                    wx.hideLoading()
+                    resolve(res.data)
+                },
+                fail: function (res) {
+                    wx.hideLoading()
+                    reject(res)
+                },
+                complete: function () {
+                    wx.hideLoading()
+                }
+                })
             })
-        })
-    }
+        }
     get(obj){
         return this.request(obj.url,'GET',obj.data)
     }
